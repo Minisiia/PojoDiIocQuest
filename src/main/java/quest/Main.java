a0451fbc94c6dac600dd3f9d2d1734c10f7cf474
Minisiia <ar_tik@yahoo.com>
2023-03-14 11:23:17 +0200

    8. Spring 1. Введення в Spring та його відмінність від Java EE

--- src/main/java/quest/Main.java
+++ src/main/java/quest/Main.java
@@ -1,6 +1,6 @@
 package quest;
 
-import config.Config;
+import quest.config.Config;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
--- src/main/java/quest/config/Config.java
+++ src/main/java/quest/config/Config.java
@@ -1,4 +1,4 @@
-package config;
+package quest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
