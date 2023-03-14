f709108287ab225714060dabe99224593cd049df
Minisiia <ar_tik@yahoo.com>
2023-03-14 15:36:45 +0200

    8. Spring 1. Введення в Spring та його відмінність від Java EE

--- src/main/java/quest/config/Config.java
+++ src/main/java/quest/config/Config.java
@@ -1,5 +1,6 @@
 package quest.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import quest.impl.CurseUndeadKing;
@@ -10,12 +11,17 @@ import quest.pojo.Knight;
 @Configuration
 public class Config {
     @Bean
-    public Quest darkForestQuest() {
+    public Quest CurseUndeadKingQuest() {
         return new CurseUndeadKing();
     }
 
     @Bean
-    public Knight knight(Quest quest) {
+    public Quest DarkForestQuest() {
+        return new DarkForest("Dark Forest: The knight ventures deep into the dark forest, where dangerous monsters and evil wizards dwell. His task is to find an ancient artifact that can save the kingdom from impending danger.");
+    }
+
+    @Bean
+    public Knight knight(@Qualifier("CurseUndeadKingQuest") Quest quest) {
         return new Knight(quest);
     }
 }
